ef21b7bffc93b220360b3537a5ad58f5c2070e87
pepesoriagarcia99 <pepesoriagarcia99@gmail.com>
2019-06-27 14:01:13 +0200

    spring

---
+++ Spring/spring-dependency-injection/src/main/java/com/journaldev/spring/di/services/EmailService.java
@@ -0,0 +1,13 @@
+package com.journaldev.spring.di.services;
+
+import org.springframework.stereotype.Service;
+
+@Service(value = "emailServiceService")
+public class EmailService implements MessageService {
+
+	public boolean sendMessage(String msg, String rec) {
+		System.out.println("Email Sent to "+rec+ " with Message="+msg);
+		return true;
+	}
+
+}
---
+++ Spring/spring-dependency-injection/src/main/java/com/journaldev/spring/di/services/TwitterService.java
@@ -0,0 +1,13 @@
+package com.journaldev.spring.di.services;
+
+import org.springframework.stereotype.Service;
+
+@Service(value="twitterService")
+public class TwitterService implements MessageService {
+
+	public boolean sendMessage(String msg, String rec) {
+		System.out.println("Twitter message Sent to "+rec+ " with Message="+msg);
+		return true;
+	}
+
+}
